202327b205dd3c34144f1f7b1ec3908d703819da
samiell <krypton7@gmail.com>
2016-03-01 10:17:22 +0000

    Minor changes

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(:version => 20160225114911) do
-=======
-ActiveRecord::Schema.define(:version => 20160224143344) do
->>>>>>> 7c0391bd9696ed8d27d5e2a6a364af725387df0e
 
   create_table "ambassador_requests", :force => true do |t|
     t.integer  "ambassador_id"
@@ -41,13 +37,6 @@ ActiveRecord::Schema.define(:version => 20160224143344) do
     t.datetime "updated_at",  :null => false
   end
 
-  create_table "campaign_countries", :force => true do |t|
-    t.integer  "country_id"
-    t.integer  "campaign_id"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
-  end
-
   create_table "campaigns", :force => true do |t|
     t.integer  "brand_id"
     t.string   "name"
@@ -64,16 +53,14 @@ ActiveRecord::Schema.define(:version => 20160224143344) do
   end
 
   create_table "countries", :force => true do |t|
-    t.string   "code"
+    t.string   "iso"
     t.string   "name"
-    t.string   "description"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
-    t.text     "iso"
-    t.text     "nicename"
-    t.text     "iso3"
-    t.text     "numcode"
-    t.text     "phonecode"
+    t.string   "nicename"
+    t.string   "iso3"
+    t.string   "numcode"
+    t.string   "phonecode"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "impressions", :force => true do |t|
@@ -130,6 +117,7 @@ ActiveRecord::Schema.define(:version => 20160224143344) do
   end
 
   create_table "users", :force => true do |t|
+    t.string   "email",                  :default => "", :null => false
     t.string   "encrypted_password",     :default => "", :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
@@ -142,11 +130,9 @@ ActiveRecord::Schema.define(:version => 20160224143344) do
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
     t.string   "role"
-    t.string   "provider"
-    t.string   "uid"
-    t.string   "email"
   end
 
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
 end
